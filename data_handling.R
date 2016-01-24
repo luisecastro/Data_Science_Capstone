6c2d9d5c5af54b261e79899302bd38fe04fcc16c
Luis Castro <luis.quiros@gmail.com>
2016-01-24 07:45:07 -0700

    added comments

--- data_handling.R
+++ data_handling.R
@@ -1,22 +1,30 @@
+#Call needed functions in "functions.R"
 source("functions.R")
 
+#Download data if not already in disk
 downloader()
+
+#Load data completely (n=1), clean data
+#and save processed data
 text <- loader(n=1) 
 text <- pre(text,ic=TRUE,ws=TRUE)
 text <- pos(text)
 save(text,file="RData/text.RData")
 
+#Split data to create 1grams, create frequency table, save
 text1 <- condition(text,n=1) 
 text1 <- tgram(text1)
 text1$ngrams <- as.character(text1$ngrams)
 save(text1,file="RData/text1.RData")
 
+#Tag start of sentences, create 2grams and frequency table, save
 text2 <- condition(text,n=2) 
 text2 <- ngram(text2,n=2)
 text2 <- tgram(text2)
 text2$ngrams <- as.character(text2$ngrams)
 save(text2,file="RData/text2.RData")
 
+#Tag start of sentences, create 3grams and frequency table, save
 text3 <- condition(text,n=3) 
 text3 <- ngram(text3,n=3)
 text3 <- tgram(text3)
@@ -24,13 +32,18 @@ text3$ngrams <- as.character(text3$ngrams)
 save(text3,file="RData/text3.RData")
 rm(text)
 
+#Strip data of ngrams whose frequency is less than 12
 m1 <- reducer(text1,n=12,m=1)
 save(m1,file="RData/m1.RData")
 
+#Trim data of unwanted ngrams
+#Strip data of ngrams which frequency is less than 6
 m2 <- trim(text2,n=2)
 m2 <- reducer(m2,n=6,m=2)
 save(m2,file="RData/m2.RData")
 
+#Trim data of unwanted ngrams
+#Strip data of ngrams which frequency is less than 3
 m3 <- trim(text3,n=3)
 m3 <- reducer(m3,n=3,m=3)
 save(m3,file="RData/m3.RData")
--- functions.R
+++ functions.R
@@ -1,8 +1,12 @@
+
+#Download data from url if not already in disk, unzip it
 downloader <- function(){
         url <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
         file <- "Coursera-SwiftKey.zip"
         if(!file.exists(file)){download.file(url=url,destfile=file);unzip(file)}
 }
+
+#sample data, merge it and load it to memory
 loader <- function(n){
         con <- file("final/en_US/en_US.news.txt",open="rb")
         news <- readLines(con, encoding="ASCII",skipNul=TRUE)
@@ -19,6 +23,8 @@ loader <- function(n){
         c(sample(news,length(news)*n),sample(blogs,length(blogs)*n),
           sample(twitter,length(twitter)*n))
 }
+
+#Clean data of digits, some punctuation, extra white spaces and other
 pre <- function(x,ic,ws){
         if(ic) x <- iconv(x,"latin1","ASCII")
         if(!ic) x <- iconv(x,"latin1","UTF-8")
@@ -29,26 +35,36 @@ pre <- function(x,ic,ws){
         if(ws) x <- gsub("^ +| +$","",x,perl=TRUE)
         x 
 }
+
+#Remove NA, empty lines and just white spaces from previous function
 pos <- function(x){
         x <- x[!is.na(x)]
         x <- x[nchar(x)>0]
         x[x!=" "]
         
 }
+
+#Tag start of sentences for future 2&3 grams, split sentences in words
 condition <- function(x,n){
         if(n==2) x <- paste("<>",x)
         if(n==3) x <- paste("<<>> <>",x)
         unlist(strsplit(x," "))
 }
+
+#create ngrams
 ngram <- function(x,n){
         len <- length(x)
         if(n==2) return(paste(x[1:(len-1)],x[2:len]))
         if(n==3) return(paste(x[1:(len-2)],x[2:(len-1)],x[3:len]))
 }
+
+#create frequency tables of ngrams, sorted decreasingly
 tgram <- function(x){
         x <- sort(table(x),decreasing=TRUE,method="quick")
         data.frame(ngrams=names(x),count=as.numeric(x))
 }
+
+#Trim tags of start of sentence and rt's from twitter
 trim <- function(x,n){
         if(n==2){
                 x <- x[-grep(" <>",x[,1],perl=TRUE),]
@@ -61,11 +77,15 @@ trim <- function(x,n){
         }
         x
 }
+
+#Visualize #of unique words and mass of words over certain frequency
 tester <- function(x,n){ 
         num <- round(sum(x[,2]>n)/nrow(x),2)
         mass <- round(sum(x[x[,2]>n,2])/sum(x[,2]),2)
         c(paste(num,"num","/",mass,"mass"))
 }
+
+#keep only ngrams in table that are greater that certain frequency
 reducer <- function(x,n,m) {
         x <- as.character(x[x[,2]>n,1])
         if(m==1) return(matrix(x,ncol=m,byrow=TRUE))
